99aff28a17576cc9ee87ed911a7acee7e5ade0a2
Ali Haroon <fa23-bcs-243@students.cuisahiwal.edu.pk>
2026-02-21 11:27:44 +0500

    Closure understanding with real world example

--- Playground/revision1/07_closure.js
+++ Playground/revision1/07_closure.js
@@ -12,3 +12,28 @@ let check = outerFunction()
 console.log(check())
 console.log(check())
 console.log(check())
+
+
+// real world example
+
+function createCounter(){
+    let count = 0;
+    let value = 10;
+    return {
+        increament: () => {
+            count++;
+            // console.log(count);
+            return count;
+        },
+        decreament: () => {
+            value--;
+            // console.log(value);
+            return value;
+        }
+    }
+}
+let checkCount = createCounter();
+console.log(checkCount.increament())
+console.log(checkCount.increament())
+console.log(checkCount.decreament())
+console.log(checkCount.decreament())
