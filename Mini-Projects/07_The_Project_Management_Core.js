e4dc795b80ac17266f27e8353b6c79e2fd2343c2
Ali Haroon <fa23-bcs-243@students.cuisahiwal.edu.pk>
2026-02-21 22:44:10 +0500

    The_Project_Management_Core Advanced System Design Proceedings

--- Mini-Projects/07_The_Project_Management_Core.js
+++ Mini-Projects/07_The_Project_Management_Core.js
@@ -1,15 +1,16 @@
+// 1st Class
 class Task{
     #status = "To Do" || "In Progress" || "Review"|| "Done"
     constructor(title,assignee){
         this.title=title,
         this.assignee=assignee
     }
-
-    get Status(){
+    
+    getStatus(){
         return this.#status
     }
 
-    set Status(state){
+    setStatus(state){
         if(this.state!=this.#status){
             console.error("Enter the Valid Status",error);
             
@@ -22,14 +23,44 @@ class Task{
 }
 
 
+// 2nd Class
 class BugTicket extends Task{
     severity= ["Low", "Medium", "Critical"]
     constructor(title,assignee,severity){
         super(title,assignee)
         this.severity=severity
     }
-
+    
     getDetails(){
-       
+       if(this.severity=="Critical"){
+        return `URGENT Bug:${this.title} | Assigne ${this.assignee} | Fix Immediately!`
+       } else{
+        return `Everything Going to be Normal ${this.severity}`
+       }
     }
 }
+
+// 3rd Class
+class ProjectBoard{
+    taskList=[]
+    constructor(taskList){
+        this.taskList=taskList
+    }
+    
+    addTicket(ticket){
+        this.taskList.push(ticket)
+    }
+    
+    getTicketsByStatus(searchStatus){
+            
+        filtered =this.taskList.filter(this.taskList.length) 
+        if(filtered==searchStatus){
+            return filtered
+        }
+        
+    }
+
+    static getCriticalCount(boardInstance){
+        
+    }
+    }
