ee3067daa45673e5b1b0aa922d082ca17dfd5b82
Ali Haroon <fa23-bcs-243@students.cuisahiwal.edu.pk>
2026-05-22 08:39:02 +0500

    Just Complete - Type Ahead: Mastery task!😃

--- JavaScript_30/06 - Type Ahead/type-ahead-mastery/script.js
+++ JavaScript_30/06 - Type Ahead/type-ahead-mastery/script.js
@@ -5,7 +5,7 @@ const names = []
 fetch(endpoint)
     .then(blob => blob.json())
     .then(data => {
-        names.push(...data)
+        names.push(...data);
         console.log("Data loaded successfully:", names);
     })
 
@@ -16,8 +16,32 @@ function findMatches(wordToMatch,names){
         // Convert everything to lowercase manually to force case-insensitivity
         const lowerSearch = wordToMatch.toLowerCase();
         const lowerName = place.name.toLowerCase();
-        const lowerEmail = place.email.toLowerCase();
+        const lowerCatchPhrase = place.company.catchPhrase.toLowerCase(); //nested data!
 
-    return lowerName.includes(lowerSearch) || lowerEmail.includes(lowerSearch);
-    });
-}
+    return lowerName.includes(lowerSearch) || lowerCatchPhrase.includes(lowerSearch);
+    }); }
+
+    //Display Function
+    function displayMatches(){
+     const matchArray = findMatches(this.value,names);
+     const html = matchArray.map( place =>{
+        return `<li>
+                <span class="name">${place.name}</span>
+                </li>`
+     }).join('')
+     
+     const suggestions = document.querySelector('.suggestions')
+     if (this.value==''){
+        suggestions.innerHTML = `
+          <li>Filter for an employee name</li>
+          <li>or a company catchphrase</li>
+        `;
+     }else { 
+     suggestions.innerHTML = html || `<li>No matches found for ${this.value}</li>`
+    }
+    };
+
+    const search = document.querySelector('.search')
+
+    search.addEventListener('change',displayMatches)
+    search.addEventListener('keyup',displayMatches)
